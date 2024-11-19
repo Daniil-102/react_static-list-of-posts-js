4bbaf2d361d66f530cff5b5724990287e073fb97
Daniel Kononchuk <11fm2020arli@gmail.com>
2024-11-19 18:02:13 +0200

    resolve task

--- src/components/CommentList/CommentList.jsx
+++ src/components/CommentList/CommentList.jsx
@@ -1,1 +1,8 @@
-export const CommentList = () => <>Put the list here</>;
+import { CommentInfo } from '../CommentInfo/CommentInfo';
+
+export const CommentList = ({ comments }) =>
+  comments.length ? (
+    comments.map(comment => <CommentInfo comment={comment} key={comment.id} />)
+  ) : (
+    <b data-cy="NoCommentsMessage">No comments yet</b>
+  );
